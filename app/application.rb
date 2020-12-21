4e22101d6f2dff1eddda3d739735c775e6698fe5
baldo-vela <baldo.vela+github@gmail.com>
2020-12-21 16:12:29 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,29 @@
+class Application
+
+    @@items = [] 
+
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        if req.path.match(/items/)
+            item_to_find = req.path.split("/items/").last
+            item = @@items.find do |item|
+                item.name == item_to_find
+                end
+
+            if item == nil
+                resp.status = 400
+                resp.write "Item not found."
+            else 
+                resp.write item.price
+            end
+
+        else
+            resp.status = 404
+            resp.write "Route not found"
+        end
+
+        resp.finish
+    end
+end
